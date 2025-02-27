32e744d78d003f5dee5672a605141db7785ad146
Ram Prakash Pathak <133332560+RamP1432@users.noreply.github.com>
2025-02-27 09:23:00 +0530

    [Ram Prakash Pathak] : Determine the Logging Levels, Logging to Console or File, Logging Patterns.

--- main/java/com/example/employeepayrollapplication/controllers/EmployeePayRollController.java
+++ main/java/com/example/employeepayrollapplication/controllers/EmployeePayRollController.java
@@ -6,9 +6,10 @@ import com.example.employeepayrollapplication.services.EmployeeService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.*;
-
+@Slf4j
 @RestController
 @RequestMapping("/EmployeePayRoll")
 public class EmployeePayRollController {
@@ -40,5 +41,17 @@ public class EmployeePayRollController {
     public String deleteEmployee(@PathVariable int myId){
         employeeService.deleteEmployee(myId);
         return "Employee is deleted.";
+    }
+
+
+
+    @GetMapping("/log")
+    public String testLogging() {
+        log.debug("This is a DEBUG log - Only visible in dev");
+        log.info("This is an INFO log - Always visible in production");
+        log.warn("This is a WARN log - Warning messages");
+        log.error("This is an ERROR log - Critical issues");
+
+        return "Logging test completed!";
     }
 }
