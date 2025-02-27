a1af5095e4db689e97250f368cbf9d00b19720d5
Ram Prakash Pathak <133332560+RamP1432@users.noreply.github.com>
2025-02-27 09:02:59 +0530

    [Ram Prakash Pathak] : Use Lombok Library for Logging.

--- main/java/com/example/employeepayrollapplication/services/EmployeeService.java
+++ main/java/com/example/employeepayrollapplication/services/EmployeeService.java
@@ -1,6 +1,6 @@
 package com.example.employeepayrollapplication.services;
 
-import com.example.employeepayrollapplication.dto.EmployeeDTO;
+import lombok.extern.slf4j.Slf4j;
 import com.example.employeepayrollapplication.model.Employee;
 import com.example.employeepayrollapplication.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,23 +9,31 @@ import org.springframework.stereotype.Service;
 import java.util.*;
 
 @Service
+@Slf4j
 public class EmployeeService {
         @Autowired
         private EmployeeRepository employeeRepository;
 
         public List<Employee> getAllEmployees() {
+            log.info("Accessing all record of Employees.");
             return employeeRepository.findAll();
         }
 
         public Optional<Employee> getEmployeeById(int id) {
-            return employeeRepository.findById(id);
+
+            return Optional.ofNullable(employeeRepository.findById(id).orElseThrow(() -> {
+                log.error("Employee not found with ID: {}", id);
+                return new RuntimeException("Employee not found");
+            }));
         }
 
         public Employee addEmployee(Employee employee) {
+            log.info("Adding new employee: {}", employee.getName());
             return employeeRepository.save(employee);
         }
 
         public Employee updateEmployee(int id, Employee employee) {
+           log.warn("Updating employee with ID: {}", id);
             Employee employee1 = employeeRepository.findById(id)
                     .orElseThrow(() -> new RuntimeException("Employee not found with ID: " + id));
             employee1.setSalary(employee.getId());
@@ -35,6 +43,7 @@ public class EmployeeService {
         }
 
         public void deleteEmployee(int id) {
+            log.info("Deleting employee with ID: {}", id);
             employeeRepository.deleteById(id);
         }
 }
